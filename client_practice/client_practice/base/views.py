4abf7ab05c530d0f7682d28ca4134104510b2ea8
Briick <howard.felix@outlook.com>
2024-02-26 16:26:48 +0700

    version1.3

---
+++ client_practice/client_practice/base/views.py
@@ -0,0 +1,274 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, Message
+from .forms import RoomForm, UserForm
+import textwrap
+import google.generativeai as genai
+from IPython.display import Markdown
+
+
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username or Password is incorrect")
+
+    context = {
+        'page': page,
+    }
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occured during registration")
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'base/login_register.html', context)
+
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q')!=None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()[0:5]
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {
+        'rooms': rooms, 'topics': topics, 'room_count': room_count,
+        'room_messages': room_messages,
+    }
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body'),
+        )
+
+        ai_response = reply(message.body)
+
+        ai_message_obj = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=ai_response,
+        )
+
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {
+        'room': room, 'room_messages': room_messages,
+        'participants': participants,
+    }
+    return render(request, 'base/room.html', context)
+
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {
+        'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics,
+
+    }
+    return render(request, 'base/profile.html', context)
+
+
+@login_required(login_url='login')
+def createRoom(request):
+    form = RoomForm()
+    topics = Topic.objects.all()
+
+    try:
+        if request.method == 'POST':
+            topic_name = request.POST.get('topic')
+            topic, created = Topic.objects.get_or_create(name=topic_name)
+
+            new_room = Room.objects.create(
+                        host=request.user,
+                        topic=topic,
+                        name=request.POST.get('name'),
+                        description=request.POST.get('description'),
+                        )
+
+            desc = request.POST.get('description')
+            ai_response = reply(desc)
+
+            ai_message_obj = Message.objects.create(
+                user=request.user,
+                room=new_room,
+                body=ai_response,
+            )
+            room_url = new_room.get_absolute_url()
+            return redirect(room_url)
+
+        context = {
+            'form': form, 'topics': topics,
+        }
+
+        return render(request, 'base/room_form.html', context)
+
+    except ValueError as e:
+        error_message = f"ValueError: ticket {str(e)}"
+        messages.error(request, error_message)
+        return redirect('create-room')
+
+
+@login_required(login_url='login')
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    topics = Topic.objects.all()
+
+    if request.user != room.host and not request.user.is_staff:
+        return HttpResponse("You do not have permission to update this room")
+
+    if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+
+    context = {
+        'form': form, 'topics': topics, 'room': room,
+    }
+    return render(request, 'base/room_form.html', context)
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user and not request.user.is_staff:
+        return HttpResponse("You do not have permission to update this room")
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    context = {
+        'obj': message,
+    }
+    return render(request, 'base/delete.html', context)
+
+@login_required(login_url='login')
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.user != room.host and not request.user.is_staff:
+        return HttpResponse("You do not have permission to update this room")
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    context = {
+        'obj': room,
+    }
+    return render(request, 'base/delete.html', context)
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'base/update-user.html', context)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {
+        'topics': topics
+    }
+    return render(request, 'base/topics.html', context)
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context = {
+        'room_messages': room_messages
+    }
+    return render(request, 'base/activity.html', context)
+
+# geminooooooooo
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+genai.configure(api_key="AIzaSyBN3fXecmaX6jWABwEtxPPMS-j6Do2Ae9o")
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+response = chat.send_message("Detect the language and when you respond, answer the question and reply back in English. "
+                             "This is Yuu. Aj. Bill is my teacher. I am studying ICT301")
+
+def reply(prompt):
+    global chat
+    respond = chat.send_message(prompt)
+    return respond.text
