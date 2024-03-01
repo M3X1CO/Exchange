3b14e10b18cb103906b2cff6e27c8413932ffd89
Howard Felix <122615166+M3X1CO@users.noreply.github.com>
2024-03-01 10:44:06 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+'''
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -102,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+'''
 
 
 # Internationalization
